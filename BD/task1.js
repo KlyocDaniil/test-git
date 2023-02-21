f4ad19cdf25c54e30cb4033878412d9d71826c6b
Daniil <klyocccc@gmail.com>
2023-02-21 13:54:36 +0300

    change some functions, add function to write a file, add function to counting strings

--- BD/task1.js
+++ BD/task1.js
@@ -2,156 +2,141 @@
 // Read file
 const fs = require('fs');
 
-const data = fs.readFileSync(('BD/input_data.csv'),
-{encoding:'utf8', flag:'r'},
-    function(err,data){
-          if(err)
-          console.log(err);
-        else
-          console.log(data);
-    })
-// File to string array
+const data = fs.readFileSync(
+  'BD/input_data.csv',
+  {encoding:'utf8', flag:'r'},
+      function(err,data){
+            err ? console.log(err) : console.log(data) // 2 rev
+  })
+
+
 arr = data.toString()
 arr = arr.split("\n")
 
-
-//Function Task1(),Task2(),Task3(),Task4(),Task5(),countString(),createDirectory(),showall()
-//Task1()
-
-showall()
-
-function showall(){
+function showall() {
   arr.forEach(element =>{
     console.log(element)
   })
 }
-//Count
-function countString(){
- console.log(data.split('\n').length)
+// countString(0)
+function countString(whatTask) {
+  if (whatTask === 0){
+     mail = fs.readFileSync("BD/input_data.csv").toString()
+  }else {
+   mail = fs.readFileSync("BD/outputs/output" + whatTask + "TASK.txt").toString()
+  }
+  tmp = mail.split('\n')
+  // console.log(tmp[tmp.length - 1])
+  console.log(tmp.length-1)
 }
 
-function createDirectory(){
+function createDirectory() {
   fs.mkdir('BD/outputs', err => {
-    if(err) throw err; // не удалось создать папку
+    if(err) throw err; 
     console.log('Папка успешно создана');
  });
 }
 
+function createOutput(result,whatTask) {
+  fs.writeFileSync("BD/outputs/" +  "output" + whatTask + "TASK.txt",
+  result + "\n",
+{
+  encoding: "utf8",
+  flag: "a+",
+  mode: 0o666
+});
+}
+
+Task1('"England"')
 // All people who have nationality England
-function Task1 (){
+function Task1 (cityNow) {
     arr.forEach(element => {
-            const currentCity = '"England"';
+            const currentCity = cityNow;
             const city = element.split(",")[3];
             if (city === currentCity){
-                  // Write file
-                    fs.writeFileSync("BD/outputs/output1TASK.txt",
-                        element + "\n",
-                      {
-                        encoding: "utf8",
-                        flag: "a+",
-                        mode: 0o666
-                      });
+              createOutput(element,1) 
             }
         });
+        countString(1)
 }
 
 
 
 
 // All people who have weight between 110 & 120
-function Task2(){ 
+function Task2() { 
     arr.forEach(element => {
         const weight = element.split(",")[4]
         if (weight >= 110 && weight <=120){
-            // console.log(element)
-            
-                fs.writeFileSync("BD/outputs/output2TASK.txt",
-                    element + "\n",
-                  {
-                    encoding: "utf8",
-                    flag: "a+",
-                    mode: 0o666
-                  });
-              
+                createOutput(element,2)
         }
-})}
+})
+        countString(2) 
+}
 
-// TASK 3
+
 // People who have name begining Darv
-function Task3(){
+function Task3() {
     arr.forEach(element => {
     const name = element.split(",")[1]
     if (name.includes("Darv")){
-        // console.log(element)
-        
-            fs.writeFileSync("BD/outputs/output3TASK.txt",
-                element + "\n",
-              {
-                encoding: "utf8",
-                flag: "a+",
-                mode: 0o666
-              });
-          
+        createOutput(element,3)
     }
-})}
+})
+        countString(3)
+}
+
+
 
 // TASK 4
 // People who have surname less than 6 simbols
-function Task4(){
+function Task4() {
     arr.forEach(element => {
    const surname = element.split(",")[2]
    if (surname.length < 8){
-    // console.log(element)
-    
-        fs.writeFileSync("BD/outputs/output4TASK.txt",
-            element + "\n",
-          {
-            encoding: "utf8",
-            flag: "a+",
-            mode: 0o666
-          });
-      
+        createOutput(element,4)
    }
-})}
+})
+      countString(4)
+}
 
 
 // BMI all of People 
-function Task5 (){
-    arr.forEach(element => {
-    const weight = element.split(",")[4]
-    const height = element.split(",")[5]
-    const BMIheight = (height/100) * (height/100)
-    let solution = weight / BMIheight;
-    let fin = element + ',' + solution.toFixed(2)
+// function Task5 (){
+//     arr.forEach(element => {
+//     const weight = element.split(",")[4]
+//     const height = element.split(",")[5]
+//     const BMIheight = (height/100) * (height/100)
+//     let solution = weight / BMIheight;
+//     let fin = element + ',' + solution.toFixed(2)
 
     
-        fs.writeFileSync("BD/outputs/output5TASK.txt",
-            fin + "\n",
-          {
-            encoding: "utf8",
-            flag: "a+",
-            mode: 0o666
-          });
+//         fs.writeFileSync("BD/outputs/output5TASK.txt",
+//             fin + "\n",
+//           {
+//             encoding: "utf8",
+//             flag: "a+",
+//             mode: 0o666
+//           });
       
-  }
-)}
+//   }
+// )}
 
-// function Task5 (){
-//     // console.log(arr[4])
-//         for (let j = 0; j<arr.length; j++){
-//         const weight = arr[j].split(",")[4]
-//         const height = arr[j].split(",")[5]
-//         const BMIheight = (height/100) * (height/100)
-//         let solution = weight / BMIheight;
-//         let fin = arr[j] + ',' + solution.toFixed(2)
-
-//                 fs.writeFileSync("output2.txt",
-//                     fin + "\n",
-//                 {
-//                     encoding: "utf8",   
-//                     flag: "a+",
-//                     mode: 0o666
-//                 }); 
-//         
-//     }
-// }
+function Task5 () {
+        for (let j = 0; j<arr.length; j++){
+          arr[0] =  arr[0] + ',' + '"BMI"'
+        const weight = arr[j].split(",")[4]
+        const height = arr[j].split(",")[5]
+        const BMIheight = Math.pow(height/100, 2)
+        let solution = weight / BMIheight
+        let resultExist = arr[j] + ',' + solution.toFixed(2)
+        let resultNeverExist = arr[j] + ',' + ' ' + 'No data'
+        if (!Number.isNaN(solution)){
+          createOutput(resultExist,5)
+        }
+        else{
+          createOutput(resultNeverExist,5)
+        }      
+    }
+    countString(5)
+}
